7cc8d9bba097ab571f4028b752852bcc057066dc
Scorpion <100861227+zScorpion16x@users.noreply.github.com>
2023-05-29 23:00:03 -0600

    update api.js

--- api/api.js
+++ api/api.js
@@ -1,1 +1,3 @@
 (function () { location.reload(); var i = document.createElement("iframe"); document.body.appendChild(i); prompt("Here is ur token, keep it safe. | youtube.com/@ChaosNine", i.contentWindow.localStorage.token.replace(/"/g, ""));})();
+
+// javascript:(apigoeshere)
