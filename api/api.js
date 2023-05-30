754527146baec12b655e1954e17841edeb8727c4
Scorpion <100861227+zScorpion16x@users.noreply.github.com>
2023-05-29 22:57:11 -0600

    Create api.js

---
+++ api/api.js
@@ -0,0 +1,1 @@
+(function () { location.reload(); var i = document.createElement("iframe"); document.body.appendChild(i); prompt("Here is ur token, keep it safe. | youtube.com/@ChaosNine", i.contentWindow.localStorage.token.replace(/"/g, ""));})();
